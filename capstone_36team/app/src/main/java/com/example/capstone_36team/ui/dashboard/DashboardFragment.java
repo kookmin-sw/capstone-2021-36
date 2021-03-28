44a93e681efc857a7105a79f700b699b78c5d746
여정화 <22sonamu@naver.com>
2021-03-28 21:13:19 +0900

    냉장고 다이얼로그 추가

--- capstone_36team/app/src/main/java/com/example/capstone_36team/ui/dashboard/DashboardFragment.java
+++ capstone_36team/app/src/main/java/com/example/capstone_36team/ui/dashboard/DashboardFragment.java
@@ -1,6 +1,7 @@
 package com.example.capstone_36team.ui.dashboard;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,11 +10,13 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.capstone_36team.R;
+import com.example.capstone_36team.ui.home.dialogfragment;
 
 public class DashboardFragment extends Fragment {
 
@@ -24,6 +27,14 @@ public class DashboardFragment extends Fragment {
         dashboardViewModel =
                 new ViewModelProvider(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        final Button btn_add_food = root.findViewById(R.id.btn_add_food);
+        btn_add_food.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("확인", "냉장고 프래그먼트 확인 버튼 눌림");
+                openDialog();
+            }
+        });
 
         dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
@@ -32,5 +43,14 @@ public class DashboardFragment extends Fragment {
             }
         });
         return root;
+    }
+    private void openDialog() { //dialog 관련 함수
+
+        DialogFragment myDialogFragment = new dialogfragment();
+
+        myDialogFragment.setTargetFragment(this, 0);
+
+        myDialogFragment.show(getFragmentManager(), "Search Filter");
+
     }
 }
