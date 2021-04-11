cb340c15c0b367ccf9acb003fc884efdf3ec5b01
JungMinAn <qqqwkd98@gmail.com>
2021-04-12 04:58:32 +0900

    add Product del and search

--- capstone_36team/app/src/main/java/com/example/capstone_36team/Room.java
+++ capstone_36team/app/src/main/java/com/example/capstone_36team/Room.java
@@ -1,10 +1,14 @@
+
 package com.example.capstone_36team;
+import com.example.capstone_36team.Product;
 
 import java.util.ArrayList;
 
 public class Room {
     String name;
-    ArrayList Plist = new ArrayList();
+
+    ArrayList<Product> Plist = new ArrayList<Product>();
+    Product p;
     public Room(String rname){
         name = rname;
     }
@@ -17,5 +21,22 @@ public class Room {
     public void addProduct(Product p){
         Plist.add(p);
     }
-    public void delProduct(){}
+    public void delProduct(String dname){
+        for(int i=0;i<Plist.size();i++){
+            p = Plist.get(i);
+            String str = Plist.get(i).getName();
+            if(str.equals(dname)) {
+                Plist.remove(i);
+            }
+        }
+    }
+    public Product searchProduct(String sname){
+        for(int i=0;i<Plist.size();i++){
+            String str = Plist.get(i).getName();
+            if(str.equals(sname)){
+                return Plist.get(i);
+            };
+        }
+        return null;
+    };
 }
