ddef0ddac188c67400b4440730f00c5caace25b3
KANGKYUNGMIN <kkgm94@naver.com>
2021-05-20 07:42:19 +0900

    change button color,bottomnavigation handler

--- capstone_36team/app/src/main/java/com/example/capstone_36team/MainActivity.java
+++ capstone_36team/app/src/main/java/com/example/capstone_36team/MainActivity.java
@@ -1,21 +1,22 @@
 package com.example.capstone_36team;
 
-import android.Manifest;
 import android.os.Bundle;
-import android.view.Menu;
+import android.widget.Toast;
 
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
+import com.example.capstone_36team.ui.dashboard.DashboardFragment;
+import com.example.capstone_36team.ui.home.HomeFragment;
+import com.example.capstone_36team.ui.notifications.NotificationsFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.gun0912.tedpermission.TedPermission;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import static android.icu.text.DisplayContext.LENGTH_SHORT;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -24,22 +25,53 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
+        MeowBottomNavigation navView = findViewById(R.id.bottomNavigation);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupWithNavController(navView, navController);
 
+        navView.add(new MeowBottomNavigation.Model(1,R.drawable.ic_room));
+        navView.add(new MeowBottomNavigation.Model(2,R.drawable.ic_food));
+        navView.add(new MeowBottomNavigation.Model(3,R.drawable.ic_info));
 
+        navView.setOnClickMenuListener(new MeowBottomNavigation.ClickListener() {
+            @Override
+            public void onClickItem(MeowBottomNavigation.Model item) {
+            }
+        });
 
+        navView.setOnShowListener(new MeowBottomNavigation.ShowListener() {
+            @Override
+            public void onShowItem(MeowBottomNavigation.Model item) {
 
+                Fragment fragment = null;
+                switch (item.getId()){
+                    case 1:
+                        fragment = new HomeFragment();
+                        break;
+
+                    case 2:
+                        fragment = new DashboardFragment();
+                        break;
+
+                    case 3:
+                        fragment = new NotificationsFragment();
+                        break;
+                }
+
+                loadFragment(fragment);
+
+            }
+        });
+        navView.show(1,true);
     }
 
+    private void loadFragment(Fragment fragment) {
 
-
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.nav_host_fragment,fragment)
+                .commit();
+    }
 
 
 }
