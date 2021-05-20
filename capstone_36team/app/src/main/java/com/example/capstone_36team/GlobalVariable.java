2b7043241632dc8a69655fd700d221ee5aff6775
JungMinAn <qqqwkd98@gmail.com>
2021-05-21 07:18:02 +0900

    user id 전역변수 추가

--- capstone_36team/app/src/main/java/com/example/capstone_36team/GlobalVariable.java
+++ capstone_36team/app/src/main/java/com/example/capstone_36team/GlobalVariable.java
@@ -4,11 +4,18 @@ import android.app.Application;
 
 public class GlobalVariable extends Application {
     private String familyid;
+    private String uid;
 
-    public String getId() {
+    public String getfamilyId() {
         return familyid;
     }
-    public void setId(String id) {
+    public void setfamilyId(String id) {
         this.familyid = id; 
+    }
+    public String getuId() {
+        return uid;
+    }
+    public void setuId(String id) {
+        this.uid = id;
     }
 }
